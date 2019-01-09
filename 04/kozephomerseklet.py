7378d5eba25fcc694068fa54cfcc9a5f6f697490
b9nc9 <b9nc9@protonmail.com>
2019-01-09 19:24:40 +0100

    újraírva

--- 04/kozephomerseklet.py
+++ 04/kozephomerseklet.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from collections import OrderedDict
+
 #sor: év#hónap#nap#óra#perc#-#hőmérséklet#páratartalom%
 meres = {}
 with open('meres.txt') as f:
@@ -7,17 +9,13 @@ with open('meres.txt') as f:
     for x in sorok:
         sor = x.split('#')
         kulcs = sor[0]+'-'+sor[1]+'-'+sor[2]
-        meres[kulcs] = []
-
-with open('meres.txt') as f:
-    sorok = f.read().splitlines()
-    for x in sorok:
-        sor = x.split('#')
-        kulcs = sor[0]+'-'+sor[1]+'-'+sor[2]
-        meres[kulcs].append(float(sor[6]))
+        if(kulcs not in meres):
+            meres[kulcs] = []
+        else:
+            meres[kulcs].append(float(sor[6]))
 
 ki = open("kimenet.txt", "w")
-for x in meres:
-    ki.write('{0} - {1:1.3f}\n'.format(x, sum(meres[x])/float(len(meres[x]))))
 
+for x in sorted(meres.keys()):
+    ki.write('{0} - {1:1.3f}\n'.format(x, sum(meres[x])/float(len(meres[x]))))
 ki.close()
